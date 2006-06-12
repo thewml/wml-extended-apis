cbe5015827121b42bfd986db23b5cf1e37aa5d6b
shlomif <shlomif@7081e830-ea04-0410-ae62-fb6148e2d9db>
2006-06-12 16:15:19 +0000

    Now the wml file is generated using Template Toolkit. Commited the other changes.

--- xhtml/1.x/Install.bash
+++ xhtml/1.x/Install.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+perl compile.pl
 dest_dir="$HOME/apps/wml/xhtml/1.x/std/"
 mkdir -p "$dest_dir"
 cp std/toc/toc.wml "$dest_dir"
---
+++ xhtml/1.x/compile.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Template;
+
+my @files = ("std/toc/toc");
+
+sub get_file_mtime
+{
+    my $path = shift;
+
+    return (stat($path))[9];
+}
+
+foreach my $f (@files)
+{
+    my $wml_f = "$f.wml";
+    my $wml_source = "$wml_f.tt2";
+    my $src_mtime = get_file_mtime($wml_source);
+    my $dest_mtime = get_file_mtime($wml_f);
+    if ((! -e $wml_f) ||
+        ($src_mtime > $dest_mtime)
+       )
+    {
+        my $t = Template->new();
+        $t->process($wml_source, +{}, $wml_f);
+    }
+}
+
