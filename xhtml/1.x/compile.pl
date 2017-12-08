6488a629fc66d8048cf4cb275163f6597e7a47d1
Shlomi Fish <shlomif@shlomifish.org>
2017-12-08 20:32:01 +0200

    pass thru perltidy to settle travis-CI.

--- xhtml/1.x/compile.pl
+++ xhtml/1.x/compile.pl
@@ -11,21 +11,20 @@ sub get_file_mtime
 {
     my $path = shift;
 
-    return (stat($path))[9];
+    return ( stat($path) )[9];
 }
 
 foreach my $f (@files)
 {
-    my $wml_f = "$f.wml";
+    my $wml_f      = "$f.wml";
     my $wml_source = "$wml_f.tt2";
-    my $src_mtime = get_file_mtime($wml_source);
+    my $src_mtime  = get_file_mtime($wml_source);
     my $dest_mtime = get_file_mtime($wml_f);
-    if ((! -e $wml_f) ||
-        ($src_mtime > $dest_mtime)
-       )
+    if (   ( !-e $wml_f )
+        || ( $src_mtime > $dest_mtime ) )
     {
         my $t = Template->new();
-        $t->process($wml_source, +{}, $wml_f);
+        $t->process( $wml_source, +{}, $wml_f );
     }
 }
 
